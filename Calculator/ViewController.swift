d0b126ae317cb5e7c1704be347878dd2395d8a65
Akihiro Matsuyama <a@art.jp>
2022-06-19 16:50:48 +0900

    ver.2

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,83 +9,105 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var CalculationProcessView: UILabel!
+    @IBOutlet weak var calculationResultView: UILabel!
     
-    @IBOutlet weak var CalculationResultView: UILabel!
-    
-    var calclationprocess = ""
-    var calclationresult = ""
+    var calculationProcess = ""
+    var calculationSymbol = ""
+    var calculationResult:Int = 0
 
     @IBAction func clearButton(_ sender: Any) {
-        calclationprocess = ""
-        calclationresult = ""
-        CalculationProcessView.text = calclationprocess
-        CalculationResultView.text = "0"
+        calculationProcess = ""
+        calculationResult = 0
+        calculationResultView.text = String(calculationResult)
     }
     
     @IBAction func plusButton(_ sender: Any) {
-        calclationprocess += "+"
-        CalculationProcessView.text = calclationprocess
+        if calculationSymbol == "+" {
+            calculationResult += Int(calculationProcess)!
+            calculationProcess = ""
+        } else if calculationSymbol == "-" {
+            calculationResult -= Int(calculationProcess)!
+            calculationProcess = ""
+        } else {
+            calculationResult = Int(calculationProcess)!
+            calculationProcess = ""
+        }
+        calculationSymbol = "+"
+        calculationResultView.text = String(calculationResult)
     }
     
     @IBAction func minusButton(_ sender: Any) {
-        calclationprocess += "-"
-        CalculationProcessView.text = calclationprocess
+        if calculationSymbol == "+" {
+            calculationResult += Int(calculationProcess)!
+            calculationProcess = ""
+        } else if calculationSymbol == "-" {
+            calculationResult -= Int(calculationProcess)!
+            calculationProcess = ""
+        } else {
+            calculationResult = Int(calculationProcess)!
+            calculationProcess = ""
+        }
+        calculationSymbol = "-"
+        calculationResultView.text = String(calculationResult)
     }
     
     @IBAction func equalButton(_ sender: Any) {
-        var calclationresult = Int(calclationprocess)
-        CalculationResultView.text = String(calclationresult)
+        if calculationSymbol == "+" {
+            calculationResult += Int(calculationProcess)!
+        } else if calculationSymbol == "-" {
+            calculationResult -= Int(calculationProcess)!
+        }
+        calculationResultView.text = String(calculationResult)
     }
     
     @IBAction func zeroButton(_ sender: Any) {
-        calclationprocess += "0"
-        CalculationProcessView.text = calclationprocess
+        calculationProcess += "0"
+        calculationResultView.text = String(calculationProcess)
     }
     
     @IBAction func oneButton(_ sender: Any) {
-        calclationprocess += "1"
-        CalculationProcessView.text = calclationprocess
+        calculationProcess += "1"
+        calculationResultView.text = String(calculationProcess)
     }
     
     @IBAction func twoButton(_ sender: Any) {
-        calclationprocess += "2"
-        CalculationProcessView.text = calclationprocess
+        calculationProcess += "2"
+        calculationResultView.text = String(calculationProcess)
     }
     
     @IBAction func threeButton(_ sender: Any) {
-        calclationprocess += "3"
-        CalculationProcessView.text = calclationprocess
+        calculationProcess += "3"
+        calculationResultView.text = String(calculationProcess)
     }
     
     @IBAction func fourButton(_ sender: Any) {
-        calclationprocess += "4"
-        CalculationProcessView.text = calclationprocess
+        calculationProcess += "4"
+        calculationResultView.text = String(calculationProcess)
     }
     
     @IBAction func fiveButton(_ sender: Any) {
-        calclationprocess += "5"
-        CalculationProcessView.text = calclationprocess
+        calculationProcess += "5"
+        calculationResultView.text = String(calculationProcess)
     }
     
     @IBAction func sixButton(_ sender: Any) {
-        calclationprocess += "6"
-        CalculationProcessView.text = calclationprocess
+        calculationProcess += "6"
+        calculationResultView.text = String(calculationProcess)
     }
     
     @IBAction func sevenButton(_ sender: Any) {
-        calclationprocess += "7"
-        CalculationProcessView.text = calclationprocess
+        calculationProcess += "7"
+        calculationResultView.text = String(calculationProcess)
     }
     
     @IBAction func eightButton(_ sender: Any) {
-        calclationprocess += "8"
-        CalculationProcessView.text = calclationprocess
+        calculationProcess += "8"
+        calculationResultView.text = String(calculationProcess)
     }
     
     @IBAction func nineButton(_ sender: Any) {
-        calclationprocess += "9"
-        CalculationProcessView.text = calclationprocess
+        calculationProcess += "9"
+        calculationResultView.text = String(calculationProcess)
     }
     
     
